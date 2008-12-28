0edbc0577125c391ae96d5f2a98bebc26b9410f1
Noel Rappin <noelrappin@gmail.com>
2008-12-28 16:45:50 -0600

    refactored set status date

--- app/models/status_report.rb
+++ app/models/status_report.rb
@@ -1,4 +1,10 @@
 class StatusReport < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  
+  before_save :set_status_date
+
+  def set_status_date
+    self.status_date = Date.today if status_date.nil?
+  end
 end
--- test/unit/status_report_test.rb
+++ test/unit/status_report_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class StatusReportTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "saving a status report saves the status date" do
+    actual = StatusReport.new
+    actual.save
+    assert_equal(Date.today.to_s, actual.status_date.to_s)
   end
+  
+  test "saving a status report that already has a date doesn't override" do
+    actual = StatusReport.new(:status_date => 10.days.ago)
+    actual.save
+    assert_equal(10.days.ago.to_s, actual.status_date.to_s)
+  end
+  
 end
