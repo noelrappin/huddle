73a34296871c1108838657b75cdc939a370596fa
Noel Rappin <noelrappin@gmail.com>
2008-12-28 16:37:33 -0600

    adding initial scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :status_reports
+
+  map.resources :projects
+
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
